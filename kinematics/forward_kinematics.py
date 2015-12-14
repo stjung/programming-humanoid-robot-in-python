4cd13ec8f2e52e4e55cca9330238708c5b76c207
sjunghans <stefan.junghans@campus.tu-berlin.de>
2015-12-14 21:29:25 +0100

    fixed the fork hopefully

--- kinematics/forward_kinematics.py
+++ kinematics/forward_kinematics.py
@@ -20,10 +20,14 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'joint_control'))
 
-from numpy.matlib import matrix, identity
+from numpy.matlib import identity
 
 from angle_interpolation import AngleInterpolationAgent
 
+from math import sin,cos
+
+from sympy import sin,cos
+from numpy import matrix
 
 class ForwardKinematicsAgent(AngleInterpolationAgent):
     def __init__(self, simspark_ip='localhost',
@@ -35,15 +39,65 @@ class ForwardKinematicsAgent(AngleInterpolationAgent):
         self.transforms = {n: identity(4) for n in self.joint_names}
 
         # chains defines the name of chain and joints of the chain
-        self.chains = {'Head': ['HeadYaw', 'HeadPitch']
+        self.chains = {'Head': ['HeadYaw', 'HeadPitch'],
                        # YOUR CODE HERE
+                       'LArm': ['LShoulderPitch', 'LShoulderRoll','LElbowYaw', 'LElbowRoll', 'LWristYaw'],
+                       'LLeg': ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'LAnklePitch', 'LAnkleRoll'],
+                       'RLeg': ['RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch', 'RAnklePitch', 'RAnkleRoll'],
+                       'RArm': ['RShoulderPitch', 'RShoulderRoll','RElbowYaw', 'RElbowRoll', 'RWristYaw']
                        }
-
+        
+        
+        # around z
+        self.r_yaw = lambda a, x, y, z: matrix(  [[cos(a), -sin(a), 0, x],
+                                                  [sin(a),  cos(a), 0, y],
+                                                  [0,       0,      1, z],
+                                                  [0,       0,      0, 1]])
+        
+        #around x
+        self.r_roll = lambda a, x, y, z: matrix( [[1, 0,      0,       x],
+                                                  [0, cos(a), -sin(a), y],
+                                                  [0, sin(a), cos(a),  z],
+                                                  [0, 0,      0,       1]])
+        
+        # around y
+        self.r_pitch = lambda a, x, y, z: matrix([[cos(a), 0, -sin(a), x],
+                                                  [0,      1, 0,       y],
+                                                  [sin(a), 0, cos(a),  z],
+                                                  [0,      0, 0,       1]])
+        
+        
+        self.tParam =          {'HeadYaw': [0,0,0], 
+                                'HeadPitch': [0,0,0],
+                                'LShoulderPitch': [0,0,0], 
+                                'LShoulderRoll': [0,0,0],
+                                'LElbowYaw': [105,15,0], 
+                                'LElbowRoll': [0,0,0], 
+                                'LWristYaw': [55,95,0,0],
+                                'RShoulderPitch': [0,0,0], 
+                                'RShoulderRoll': [0,0,0],
+                                'RElbowYaw': [105,-15,0], 
+                                'RElbowRoll': [0,0,0], 
+                                'RWristYaw': [55,-95,0,0],
+                                'LHipYawPitch': [0,0,0], 
+                                'LHipRoll': [0,0,0], 
+                                'LHipPitch': [0,0,0], 
+                                'LKneePitch': [0,0,-100],  
+                                'LAnkleRoll': [0,0,0],
+                                'LAnklePitch': [0,0,-102.9],
+                                'RHipYawPitch': [0,0,0], 
+                                'RHipRoll': [0,0,0], 
+                                'RHipPitch': [0,0,0], 
+                                'RKneePitch': [0,0,-100], 
+                                'RAnkleRoll': [0,0,0], 
+                                'RAnklePitch': [0,0,-102.9]
+                                }
+        
     def think(self, perception):
         self.forward_kinematics(perception.joint)
         return super(ForwardKinematicsAgent, self).think(perception)
 
-    def local_trans(self, joint_name, joint_angle):
+    def local_trans(self, joint_name, joint_angle, symbolic={}):
         '''calculate local transformation of one joint
 
         :param str joint_name: the name of joint
@@ -51,12 +105,28 @@ class ForwardKinematicsAgent(AngleInterpolationAgent):
         :return: transformation
         :rtype: 4x4 matrix
         '''
-        T = matrix()
+        #T = matrix()
         # YOUR CODE HERE
+        
+        if not joint_name in symbolic.keys():
+            params = self.tParam[joint_name]
+        else:
+            params = self.tParam[joint_name]
+            for i in xrange(len(symbolic[joint_name][1:])):
+                if params[i] != 0:
+                    params[i] = symbolic[joint_name][i]
+            joint_angle = symbolic[joint_name][0]
+            
+        if joint_name.endswith("Yaw"):
+            return self.r_yaw(joint_angle, params[0], params[1], params[2])
+        elif joint_name.endswith("Pitch"):
+            return self.r_pitch(joint_angle, params[0], params[1], params[2])
+        elif joint_name.endswith("Roll"):
+            return self.r_roll(joint_angle, params[0], params[1], params[2])
+        
+        return None
 
-        return T
-
-    def forward_kinematics(self, joints):
+    def forward_kinematics(self, joints, symbolic={}):
         '''forward kinematics
 
         :param joints: {joint_name: joint_angle}
@@ -64,10 +134,16 @@ class ForwardKinematicsAgent(AngleInterpolationAgent):
         for chain_joints in self.chains.values():
             T = identity(4)
             for joint in chain_joints:
+                if not joint in joints.keys():
+                    print joint
+                    continue
+                
                 angle = joints[joint]
-                Tl = local_trans(joint, angle)
+                Tl = self.local_trans(joint, angle, symbolic)
                 # YOUR CODE HERE
-
+                
+                T = T*Tl
+                
                 self.transforms[joint] = T
 
 if __name__ == '__main__':
--- kinematics/inverse_kinematics.py
+++ kinematics/inverse_kinematics.py
@@ -12,7 +12,10 @@
 
 from forward_kinematics import ForwardKinematicsAgent
 from numpy.matlib import identity
-
+from math import atan2, sqrt
+from sympy import *
+import sys
+from numpy import matrix
 
 class InverseKinematicsAgent(ForwardKinematicsAgent):
     def inverse_kinematics(self, effector_name, transform):
@@ -22,21 +25,62 @@ class InverseKinematicsAgent(ForwardKinematicsAgent):
         :param transform: 4x4 transform matrix
         :return: list of joint angles
         '''
+        
+        target = from_trans(transform)
+        
+        init_printing()
+        symParams = {}
+        yawAngles = []
+        pitchAngles = []
+        rollAngles = []
+        for joint in self.chains[effector_name]:
+            a = symbols('a_' + joint)
+            
+            if joint.endswith("Yaw"):
+                yawAngles.append(a)
+            elif joint.endswith("Pitch"):
+                pitchAngles.append(a)
+            elif joint.endswith("Roll"):
+                rollAngles.append(a)
+            
+            symParams[joint] = [a]
+            symParams[joint] += [symbols('l_' + joint + '_%d' % i) for i in range(len(self.tParam[joint]))]
+            
+        
         joint_angles = []
         # YOUR CODE HERE
+        self.forward_kinematics(self.perception.joint, symbolic=symParams)
+        
+        T = self.transforms[self.chains[effector_name][-1]]
+        print latex(matrix(T))
+        #sys.setrecursionlimit(10050)
+        #simplify(T)
+        eq1 = sum(yawAngles)
+        eq2 = sum(pitchAngles)
+        eq3 = sum(rollAngles)
+        eq = T * transform.inv() - T0
+        eq = [eq1-target[3], eq2-target[4], eq3-target[5]]
+        
         return joint_angles
 
     def set_transforms(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
+        self.inverse_kinematics(effector_name, transform)
         self.keyframes = ([], [], [])  # the result joint angles have to fill in
+
+def from_trans(m):
+    # decomposition as in http://nghiaho.com/?page_id=846    
+    return [m[0, -1], m[1, -1], m[2, -1], atan2(m[2, 1], m[2, 2]), atan2(-m[2, 0], sqrt(m[2, 1]**2 +  m[2, 2]**2)), atan2(m[1, 0], m[0, 0])]
 
 if __name__ == '__main__':
     agent = InverseKinematicsAgent()
     # test inverse kinematics
     T = identity(4)
-    T[-1, 1] = 0.05
-    T[-1, 2] = 0.26
+   # T[-1, 1] = 0.05
+   # T[-1, 2] = 0.26
+    T[1, -1] = 0.05
+    T[2, -1] = 0.26
     agent.set_transforms('LLeg', T)
     agent.run()
